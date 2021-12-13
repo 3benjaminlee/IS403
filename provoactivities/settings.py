bdc911d12d314ed52b3497ecf9647e1bb55c6aa5
Benjamin <61838767+3benjaminlee@users.noreply.github.com>
2021-12-13 16:03:27 -0700

    Update settings.py

--- provoactivities/settings.py
+++ provoactivities/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r(pl_$2pygz&c+!bu5lb60i_@n0#((8e@c^l(zwyuklcmvq(g&'
+ SECRET_KEY = os.environ.get('SECRET_KEY')
+ EMAIL_HOST_USER = os.environ.get('benjaminlee24@hotmail.com')
+ EMAIL_HOST_PASSWORD = os.environ.get('admin')    
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'is403.herokuapp.com']
 
 
 # Application definition
